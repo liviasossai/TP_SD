cf0c2e57d7598ddb41dda4ebb32cdbb7e2ce25c2
Gustavo <gustavoasj@gmail.com>
2015-06-02 19:27:21 -0300

    Chamada do inicio da eleição quando ha consulta. Eu não testei

--- Cliente_Servidor/Identidade/routes/index.js
+++ Cliente_Servidor/Identidade/routes/index.js
@@ -30,42 +30,42 @@ var list_servers = [3001, 4001, 5001, 6001]; //lista de servidores
 io_server.on('connection', function(socket) {
 
 
-  socket.on('elected', function(IP_e, porta_e) {
-    console.log('Um servidor foi eleito');
-    console.log("IP eleito: " + IP_e);
-    console.log("Porta eleito: " + porta_e);
-    //IP = IP_e;
-    porta = porta_e;
+    socket.on('elected', function(IP_e, porta_e) {
+        console.log('Um servidor foi eleito');
+        console.log("IP eleito: " + IP_e);
+        console.log("Porta eleito: " + porta_e);
+        //IP = IP_e;
+        porta = porta_e;
 
-  });
+    });
 
 
-  socket.on('failed', function(num_Server) {
-    console.log('Servidor ' + num_Server + 'falhou');
+    socket.on('failed', function(num_Server) {
+        console.log('Servidor ' + num_Server + 'falhou');
 
-    if (porta == list_servers[num_Server] + 1) {
-      var rand = Math.floor(Math.random() * 4);
-      while (rand == num_Server) {
-        var rand = Math.floor(Math.random() * 4);
-      }
-      var port_client = list_servers[rand];
-      var io_client = require('socket.io-client')('http://localhost:' + port_client);
+        if (porta == list_servers[num_Server] + 1) {
+            var rand = Math.floor(Math.random() * 4);
+            while (rand == num_Server) {
+                var rand = Math.floor(Math.random() * 4);
+            }
+            var port_client = list_servers[rand];
+            var io_client = require('socket.io-client')('http://localhost:' + port_client);
 
-      //Socket cliente
-      io_client.on('connect', function() {
-        console.log("socket conectado ao servidor " + port_client + " para iniciar eleicao");
-        io_client.emit('inic_eleicao', true);
-      });
-      setTimeout(function() {
-        io_client.disconnect();
-      }, 1000);
+            //Socket cliente
+            io_client.on('connect', function() {
+                console.log("socket conectado ao servidor " + port_client + " para iniciar eleicao");
+                io_client.emit('inic_eleicao', true);
+            });
+            setTimeout(function() {
+                io_client.disconnect();
+            }, 1000);
 
-    }
-  });
+        }
+    });
 
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
-  });
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
 });
 
 
@@ -77,106 +77,105 @@ io_server.on('connection', function(socket) {
 
 function embaralhaPecas(qtd_pares) {
 
-    var imagensPossiveis = new Array();
-    var imagensPossiveis_aux = new Array();
-    var posicoesPossiveis = new Array();
+        var imagensPossiveis = new Array();
+        var imagensPossiveis_aux = new Array();
+        var posicoesPossiveis = new Array();
 
-    var posicoes;
+        var posicoes;
 
 
-    for (var i = 0; i < NUM_IMAGENS; i++) {
-      imagensPossiveis[i] = i;
+        for (var i = 0; i < NUM_IMAGENS; i++) {
+            imagensPossiveis[i] = i;
+        }
+
+        shuffle(imagensPossiveis);
+
+        for (var i = 0; i < qtd_pares; i++) {
+            imagensPossiveis_aux[imagensPossiveis_aux.length] = imagensPossiveis[i];
+        }
+
+        for (var i = 0; i < qtd_pares; i++) {
+            posicoesPossiveis[posicoesPossiveis.length] = imagensPossiveis_aux[i];
+
+        }
+        for (var i = 0; i < qtd_pares; i++) {
+            posicoesPossiveis[posicoesPossiveis.length] = imagensPossiveis_aux[i];
+
+        }
+
+        shuffle(posicoesPossiveis);
+
+        console.log("Posições das peças");
+        console.log(posicoesPossiveis);
+
+        return posicoesPossiveis;
+
+
     }
-
-    shuffle(imagensPossiveis);
-
-    for (var i = 0; i < qtd_pares; i++) {
-      imagensPossiveis_aux[imagensPossiveis_aux.length] = imagensPossiveis[i];
-    }
-
-    for (var i = 0; i < qtd_pares; i++) {
-      posicoesPossiveis[posicoesPossiveis.length] = imagensPossiveis_aux[i];
-
-    }
-    for (var i = 0; i < qtd_pares; i++) {
-      posicoesPossiveis[posicoesPossiveis.length] = imagensPossiveis_aux[i];
-
-    }
-
-    shuffle(posicoesPossiveis);
-
-    console.log("Posições das peças");
-    console.log(posicoesPossiveis);
-
-    return posicoesPossiveis;
-
-
-  }
-  //*********Fim Jogo*********//
+    //*********Fim Jogo*********//
 
 
 
 //*******Inicio Requisicao RPC********//
 router.post('/', function(req, res) {
-  var func = req.body.FUNCAO;
+    var func = req.body.FUNCAO;
 
-  if (func == 'ContarPalavra') {
-    var str = req.body.TEXTO;
-    var tam = ContarPalavra(str);
-    res.json({
-      numWord: tam
-    });
+    if (func == 'ContarPalavra') {
+        var str = req.body.TEXTO;
+        var tam = ContarPalavra(str);
+        res.json({
+            numWord: tam
+        });
 
-  } else if (func == 'embaralharPecas') {
-    console.log("Função requisitada: embaralhar peças");
-    NUM_PARES = req.body.num_pares;
+    } else if (func == 'embaralharPecas') {
+        console.log("Função requisitada: embaralhar peças");
+        NUM_PARES = req.body.num_pares;
 
-    var pecas_jogo = embaralhaPecas(NUM_PARES);
-    res.json({
-      pos: pecas_jogo
-    });
+        var pecas_jogo = embaralhaPecas(NUM_PARES);
+        res.json({
+            pos: pecas_jogo
+        });
 
-  } else if (func == 'consulta') {
-    if (porta == null) {
-      Init_election();
-      res.json({
-        ip: IP,
-        port: porta
-      });
-    } else {
-      res.json({
-        ip: IP,
-        port: porta
-      });
+    } else if (func == 'consulta') {
+
+        Init_election(res_json);
+
+        function res_json() {
+            res.json({
+                ip: IP,
+                port: porta
+            });
+        }
     }
 
-  } else if (func == 'no_server') { // Servidor lider nao responde
-    Init_election();
+} else if (func == 'no_server') { // Servidor lider nao responde
+    Init_election(res_json);
 
-  } else {
+} else {
     res.json(undefined);
-  }
+}
 });
 
 function ContarPalavra(str) {
-    var palavras = str.split(" ");
-    return palavras.length;
-  }
-  //********Fim Contar Palavra**********//
+        var palavras = str.split(" ");
+        return palavras.length;
+    }
+    //********Fim Contar Palavra**********//
 
-function Init_election() {
-  var rand = Math.floor(Math.random() * 4);
-  var port_client = list_servers[rand];
-  var io_client = require('socket.io-client')('http://localhost:' + port_client);
+function Init_election(callback) {
+    var rand = Math.floor(Math.random() * 4);
+    var port_client = list_servers[rand];
+    var io_client = require('socket.io-client')('http://localhost:' + port_client);
 
-  //Socket cliente
-  io_client.on('connect', function() {
-    console.log("socket conectado ao servidor " + port_client + " para iniciar eleicao");
-    io_client.emit('inic_eleicao', true);
-  });
-  setTimeout(function() {
-    io_client.disconnect();
-  }, 1000);
+    //Socket cliente
+    io_client.on('connect', function() {
+        console.log("socket conectado ao servidor " + port_client + " para iniciar eleicao");
+        io_client.emit('inic_eleicao', true);
+        callback();
+    });
+    setTimeout(function() {
+        io_client.disconnect();
+    }, 1000);
 }
 
 
